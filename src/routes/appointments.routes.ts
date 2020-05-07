6beb622471ecfdb1022f8ce3a4358547f5093e2b
edbrownc <edbrownc@gmail.com>
2020-05-07 13:10:00 -0700

    Add global error catching middleware to router

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -20,20 +20,16 @@ appointmentsRouter.get('/', async (request, response) => {
 appointmentsRouter.post('/', async (request, response) => {
   const { provider_id, date } = request.body;
 
-  try {
-    const parsedDate = parseISO(date);
+  const parsedDate = parseISO(date);
 
-    const createAppointment = new CreateAppointmentService();
+  const createAppointment = new CreateAppointmentService();
 
-    const appointment = await createAppointment.execute({
-      provider_id,
-      date: parsedDate,
-    });
+  const appointment = await createAppointment.execute({
+    provider_id,
+    date: parsedDate,
+  });
 
-    return response.json(appointment);
-  } catch (error) {
-    return response.status(400).json({ error: error.message });
-  }
+  return response.json(appointment);
 });
 
 export default appointmentsRouter;
