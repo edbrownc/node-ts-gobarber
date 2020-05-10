14e5b31ce2d3b3ce9a18f8ba06618bae2fe478e1
edbrownc <edbrownc@gmail.com>
2020-05-09 18:38:12 -0700

    Refactoring structure as Domain Driven Design

--- src/modules/users/services/CreateSessionService.ts
+++ src/modules/users/services/CreateSessionService.ts
@@ -1,11 +1,11 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
-import User from '../models/User';
+import User from '@modules/users/infra/typeorm/entities/User';
 
-import AppError from '../errors/AppError';
+import AppError from '@shared/errors/AppError';
 
-import authConfig from '../config/auth';
+import authConfig from '@config/auth';
 
 interface Request {
   email: string;
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,12 +3,11 @@ import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 
+import AppError from '@shared/errors/AppError';
+import uploadConfig from '@config/upload';
 import routes from './routes';
 
-import AppError from './errors/AppError';
-
-import uploadConfig from './config/upload';
-import './database';
+import '@shared/infra/typeorm';
 
 const app = express();
 
